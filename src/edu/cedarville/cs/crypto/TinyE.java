5eb07b74282370de7ef275e0f6f015b547c9bab1
Vance Royal Ehrlich <vehrlich@cedarforest.cedarville.edu>
2018-02-13 16:07:08 -0500

    Stub the encryption and drcyption algorithms

--- src/edu/cedarville/cs/crypto/TinyE.java
+++ src/edu/cedarville/cs/crypto/TinyE.java
@@ -10,10 +10,19 @@ public class TinyE {
             ArrayList<Integer> ciphertext = new ArrayList<Integer>();
             
             if (mode == Mode.ECB) {
+//                C0 = E(P0, K)
+//                C1 = E(P1, K)
+
 
             } else if (mode == Mode.CBC) {
+//                C0 = E(IV  P0, K)
+//                C1 = E(C0  P1, K)
+                
 
             } else {
+//                C0 = P0  E(IV, K)
+//                C1 = P1  E(IV+1, K)
+
 
             }
             
@@ -24,14 +33,38 @@ public class TinyE {
             ArrayList<Integer> plaintext = new ArrayList<Integer>();
             
             if (mode == Mode.ECB) {
+//                P0 = D(C0, K)
+//                P1 = D(C1, K)
+
 
             } else if (mode == Mode.CBC) {
+//                P0 = IV  D(C0, K)
+//                P1 = C0  D(C1, K)
+
 
             } else {
+//                P0 = C0  E(IV, K)
+//                P1 = C1  E(IV+1, K)
+
 
             }
             
             return (Integer[]) plaintext.toArray();
 	}
 	
+        public static Integer[] teaEncryption(Integer[] iv, Integer[] key) {
+            ArrayList<Integer> ciphertext = new ArrayList<>();
+            
+            
+            
+            return (Integer[]) ciphertext.toArray();
+        }
+        
+        public static Integer[] teaDecryption(Integer[] iv, Integer[] key) {
+            ArrayList<Integer> plaintext = new ArrayList<>();
+            
+            
+            
+            return (Integer[]) plaintext.toArray();
+        }
 }

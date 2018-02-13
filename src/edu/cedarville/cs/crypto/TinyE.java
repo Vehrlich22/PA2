812233f8492f2b1187e1d791659ef303db238d7e
Vance Royal Ehrlich <vehrlich@cedarforest.cedarville.edu>
2018-02-13 15:49:33 -0500

    Stub the encrypt and decrypt methods

--- src/edu/cedarville/cs/crypto/TinyE.java
+++ src/edu/cedarville/cs/crypto/TinyE.java
@@ -1,15 +1,37 @@
 package edu.cedarville.cs.crypto;
 
+import java.util.ArrayList;
+
 public class TinyE {
 	
 	public static enum Mode { ECB, CBC, CTR };
 	
 	public Integer[] encrypt(Integer[] plaintext, Integer[] key, Mode mode, Integer[] iv) {
-		return null;
+            ArrayList<Integer> ciphertext = new ArrayList<Integer>();
+            
+            if (mode == Mode.ECB) {
+
+            } else if (mode == Mode.CBC) {
+
+            } else {
+
+            }
+            
+            return (Integer[]) ciphertext.toArray();
 	}
 
 	public Integer[] decrypt(Integer[] ciphertext, Integer[] key, Mode mode, Integer[] iv) {
-		return null;
+            ArrayList<Integer> plaintext = new ArrayList<Integer>();
+            
+            if (mode == Mode.ECB) {
+
+            } else if (mode == Mode.CBC) {
+
+            } else {
+
+            }
+            
+            return (Integer[]) plaintext.toArray();
 	}
 	
 }
--- src/edu/cedarville/cs/crypto/Tools.java
+++ src/edu/cedarville/cs/crypto/Tools.java
@@ -3,19 +3,19 @@ package edu.cedarville.cs.crypto;
 public class Tools {
 	
 	public static Integer[] convertFromBytesToInts(byte[] bs) {
-		return null;
+            return null;
 	}
 	
 	public static Integer[] convertFromHexStringToInts(String s) {
-		return null;
+            return null;
 	}
 	
 	public static byte[] convertFromIntsToBytes(Integer[] ints) {
-		return null;
+            return null;
 	}
 	
 	public static String convertFromIntsToHexString(Integer[] ints) {
-		return null;
+            return null;
 	}
 	
 }

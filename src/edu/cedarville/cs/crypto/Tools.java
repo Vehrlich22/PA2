e4f4b731c7c3a3f03e66c89f3bf1b1a2a15a2541
Vance Ehrlich <vance.ehrlich@gmail.com>
2018-02-14 09:37:28 -0500

    Implement convertFromBytesToInts

--- src/edu/cedarville/cs/crypto/Tools.java
+++ src/edu/cedarville/cs/crypto/Tools.java
@@ -1,9 +1,43 @@
 package edu.cedarville.cs.crypto;
 
+import java.util.ArrayList;
+
 public class Tools {
 	
 	public static Integer[] convertFromBytesToInts(byte[] bs) {
-            return null;
+            ArrayList<Integer> ints = new ArrayList<>();
+            
+            for (int i = 0; i < bs.length; i+=4) {
+                byte a, b, c, d;
+                
+                if (i < bs.length) {
+                    a = (byte) (bs[i] & 0xFF);
+                } else {
+                    a = 0x00;
+                }
+                
+                if (i+1 < bs.length) {
+                    b = (byte) (bs[i + 1] & 0xFF);
+                } else {
+                    b = 0x00;
+                } 
+                
+                if (i+2 < bs.length) {
+                    c = (byte) (bs[i + 2] & 0xFF);
+                } else {
+                    c = 0x00;
+                } 
+                
+                if (i+3 < bs.length) {
+                    d = (byte) (bs[i + 3] & 0xFF);
+                } else {
+                    d = 0x00;
+                }  
+                
+                ints.add(a | b | c | d);
+            }
+            
+            return (Integer[]) ints.toArray();
 	}
 	
 	public static Integer[] convertFromHexStringToInts(String s) {
